8833f82a0830af3fcb0cdab56fdfefafc33b4f76
luigit12 <58264257+Luigit12@users.noreply.github.com>
2021-11-09 11:02:45 +0100

    Add files via upload

---
+++ scripts/rickroll.py
@@ -0,0 +1,12 @@
+import keys,time
+
+time.sleep(5)
+keys.sendSpecial('left-meta b')
+#time.sleep(1)
+keys.sendSpecial('left-ctrl t')
+#time.sleep(1)
+keys.send('youtu.be/ptCzSA1BY6s')
+#time.sleep(1)
+keys.sendSpecial('enter')
+#time.sleep(3)
+keys.send('f')
