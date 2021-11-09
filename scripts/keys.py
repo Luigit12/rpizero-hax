23083a0716eda94a2747880043cc3536df548159
luigit12 <58264257+Luigit12@users.noreply.github.com>
2021-11-09 11:02:26 +0100

    Create keys.py

---
+++ scripts/keys.py
@@ -0,0 +1,18 @@
+import os, time
+
+def send(keys):
+	for element in range(0, len(keys)):
+		if keys[element].isupper() == True:
+			os.system('/home/pi/pizero-usb-hid-keyboard/sendkeys left-shift ' + keys[element].lower())
+		elif keys[element].isupper() == False:
+            		if keys[element] == "/":
+                		os.system('/home/pi/pizero-usb-hid-keyboard/sendkeys ' + "slash")
+            		elif keys[element] == ".":
+                		os.system('/home/pi/pizero-usb-hid-keyboard/sendkeys ' + "period")
+            		else:
+                		os.system('/home/pi/pizero-usb-hid-keyboard/sendkeys ' + keys[element])
+	print(keys)
+
+def sendSpecial(keys):
+	os.system('/home/pi/pizero-usb-hid-keyboard/sendkeys ' + keys)
+	print(keys)
