0344b01269a7f7b4e0888dbb77c11947b4576ece
luigit12 <58264257+Luigit12@users.noreply.github.com>
2021-11-09 11:10:15 +0100

    Update keys.py

--- scripts/keys.py
+++ scripts/keys.py
@@ -3,12 +3,12 @@ import os, time
 def send(keys):
 	for element in range(0, len(keys)):
 		if keys[element].isupper() == True:
-			os.system('/home/pi/pizero-usb-hid-keyboard/sendkeys left-shift ' + keys[element].lower())
+			os.system('echo' + keys[element].lower() + ' | /home/pi/hid-gadget-test /dev/hidg0 keyboard')
 		elif keys[element].isupper() == False:
             		if keys[element] == "/":
-                		os.system('/home/pi/pizero-usb-hid-keyboard/sendkeys ' + "slash")
+                		os.system('echo "slash" | /home/pi/hid-gadget-test /dev/hidg0 keyboard')
             		elif keys[element] == ".":
-                		os.system('/home/pi/pizero-usb-hid-keyboard/sendkeys ' + "period")
+                		os.system('echo "period" | /home/pi/hid-gadget-test /dev/hidg0 keyboard')
             		else:
                 		os.system('/home/pi/pizero-usb-hid-keyboard/sendkeys ' + keys[element])
 	print(keys)
